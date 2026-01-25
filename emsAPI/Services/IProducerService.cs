d1a56654da393a7d0bc77009e67fc5193cbce24b
SewerynSatro <sewerynsatro@gmail.com>
2026-01-26 00:53:26 +0100

    Add ProducerService service and interface; changed variable name in Producer model

---
+++ emsAPI/Services/IProducerService.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Services;
+
+public interface IProducerService
+{
+    Task<bool> Create(Producer producer);
+    Task<Producer?> ReadOne(int id);
+    Task<List<Producer>> ReadAll();
+    Task<bool> Delete(int id);
+    Task<bool> Update(int id, Producer updatedProducer);
+}
---
+++ emsAPI/Services/ProducerService.cs
@@ -0,0 +1,58 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Services;
+
+public class ProducerService : IProducerService
+{
+    private readonly AppDbContext _context;
+
+    public ProducerService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Create(Producer producer)
+    {
+        await _context.Producers.AddAsync(producer);
+        var created = await _context.SaveChangesAsync();
+        return created > 0;
+    }
+
+    public async Task<Producer?> ReadOne(int id)
+    {
+        return await _context.Producers
+            .Include(p => p.Devices)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<List<Producer>> ReadAll()
+    {
+        return await _context.Producers
+            .Include(p => p.Devices)
+            .ToListAsync();
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        var producer = await _context.Producers.FindAsync(id);
+        if (producer == null) return false;
+
+        _context.Producers.Remove(producer);
+        var deleted = await _context.SaveChangesAsync();
+        return deleted > 0;
+    }
+
+    public async Task<bool> Update(int id, Producer updatedProducer)
+    {
+        var producer = await _context.Producers.FindAsync(id);
+        if (producer == null) return false;
+        
+        producer.Name = updatedProducer.Name;
+
+        _context.Producers.Update(producer);
+        var updated = await _context.SaveChangesAsync();
+        return updated > 0;
+    }
+}
