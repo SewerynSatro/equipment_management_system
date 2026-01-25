8c9ce56c15110f4eeadd8b583692ca9b2ca82756
szymon <szymon.pt4sznik@gmail.com>
2026-01-26 00:45:23 +0100

    added loan service interface

---
+++ emsAPI/Services/ILoanService.cs
@@ -0,0 +1,12 @@
+using Models;
+
+namespace Services;
+
+public interface ILoanService
+{
+    public Task<bool> Create(Loan loan);
+    public Task<Loan?> ReadOne(int id);
+    public Task<List<Loan>> ReadAll();
+    public Task<bool> Delete(int id);
+    public Task<bool> Update(int id, Loan updatedLoan);
+}
