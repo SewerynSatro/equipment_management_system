be939ce68b5eda867179b8202bec14278be0dfe2
SewerynSatro <sewerynsatro@gmail.com>
2026-01-31 18:08:16 +0100

    Add FluentValidation for Device

---
+++ emsAPI/DTOs/DeviceUpdateDTOvalidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Models.Dtos.Device;
+
+public class DeviceUpdateDtoValidator : AbstractValidator<DeviceUpdateDto>
+{
+    public DeviceUpdateDtoValidator()
+    {
+        RuleFor(x => x.SerialNumber)
+            .NotEmpty()
+            .WithMessage("Numer seryjny jest wymagany.")
+            .MaximumLength(100)
+            .WithMessage("Numer seryjny może mieć maksymalnie 100 znaków.")
+            .Must(sn => !string.IsNullOrWhiteSpace(sn))
+            .WithMessage("Numer seryjny nie może składać się tylko z białych znaków.")
+            .Matches(@"^[A-Za-z0-9\-_]+$")
+            .WithMessage("Numer seryjny może zawierać tylko litery, cyfry, myślniki i podkreślenia.");
+
+        RuleFor(x => x.TypeId)
+            .GreaterThan(0)
+            .WithMessage("Typ urządzenia jest wymagany.");
+
+        RuleFor(x => x.ProducerId)
+            .GreaterThan(0)
+            .WithMessage("Producent jest wymagany.");
+    }
+}
